925dd5835a8475a40d0ea5ae57a664c86e0ef4e8
Reece Olsen <olsenre17@gmail.com>
2022-03-08 09:06:31 -0700

    made the function to filter an array into even numbers

---
+++ TDD Directory/functions.swift
@@ -0,0 +1,11 @@
+//
+//  functions.swift
+//  TDD Directory
+//
+//  Created by Reece Olsen on 3/8/22.
+//
+
+import Foundation
+func evenNumbers(numbers: [Int]) -> [Int] {
+    return numbers.filter { $0 % 2 != 0 }
+}
